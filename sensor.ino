b7a46656f85faa0aea206c8541a0d05e83d20396
jul13nc <jul13nc@users.noreply.github.com>
2024-05-18 18:41:03 +0200

    First version, reading LJ18A3 sensor

---
+++ sensor.ino
@@ -0,0 +1,27 @@
+
+const int SENSOR_INPUT_PIN=2;
+// Duration of the pulse in microseconds
+unsigned long pulseDuration;
+// Minmum duration of the pulse  to be taken into account
+// Maximum flow should be around 1L/s, so 0,5s per pulse (half disk)
+// Having the minimum pulse to 0,2s should be enough to avoid false positive
+// And to be able to still read all possible values
+const unsigned long minDuration = 200000; // 0,2s
+
+const unsigned long TIMEOUT_FOR_PULSE_INPUT = 180000000;
+
+void setup() {
+    pinMode(SENSOR_INPUT_PIN, INPUT);
+    Serial.begin(9600);
+}
+ 
+void loop() {
+  pulseDuration = pulseInLong(SENSOR_INPUT_PIN, LOW, TIMEOUT_FOR_PULSE_INPUT);
+  Serial.print(pulseDuration);
+  if (pulseDuration > minDuration) {
+    Serial.println(" OUI");
+  } else {
+    Serial.println(" NON");
+  }
+  Serial.flush();
+}
